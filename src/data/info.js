33dba69185a655879d4655bb288a3d8c75e7e5b0
Oliver K. Plummer <oly.rbsc@gmail.com>
2017-03-01 22:20:19 +1100

    Updated Subsciption key

--- src/data/info.js
+++ src/data/info.js
@@ -1,7 +1,7 @@
 const info = {
 	dev: true,
 	version: '1.0.0',
-	apiKey: { 'Ocp-Apim-Subscription-Key': 'fb0f93e2d2c24ba4bd66e641a39e6910' },
+	apiKey: { 'Ocp-Apim-Subscription-Key': 'd1b5f6f7e7564997940ab1c5d611a513' },
 	date: new Date(),
 	banner: 'Please note that all stats are scrambled up to 20 +/-',
 	header: {
